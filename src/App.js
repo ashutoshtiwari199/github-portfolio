1a20e8c434afba819e42aa0a46aaed11967ec98b
Ashutosh Tiwari <ashutoshtiwari199@gmail.com>
2021-08-28 00:01:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import '../src/App.css'
+import Repos from './component/Repos'
+import Axios from 'axios'
+import Heerosection from './component/Heerosection'
+import UserPage from './component/UserPage'
 
 function App() {
+
+  const [query,setQuery]=useState('')
+  const [user, setUser]=useState(null)
+
+
+  const fetchDetails = async ()=>{
+    try {
+      const {data} = await Axios.get(`https://api.github.com/users/${query}`);
+      setUser(data)
+      console.log(data.user)
+    } catch (error) {
+      alert("No user find with this username")
+    }
+  }
+  
+  // const urlo='https://api.github.com/users/ashutoshtiwari199/repos'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <input id="inputuser" value={query} onChange={e=>setQuery(e.target.value)} placeholder="search Git User" />
+    <button id="Submit" onClick={fetchDetails} >click me </button>
+      <Heerosection />
+      {/* <UserPage/> */}
+      {user ? <UserPage user={user} />
+       : console.log("no")}
+
+      {user ? <Repos repos_url={user.repos_url}/> : null}
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
